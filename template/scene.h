4378aac4f0541bcf4aead9f68bbe3f54625f93d2
Merijn Schepers <merijnschepers@hotmail.nl>
2022-11-26 12:52:50 +0100

    working glass?

--- template/scene.h
+++ template/scene.h
@@ -254,10 +254,14 @@ public:
 		mats[1] = Material{ float4(0, 1, 0, 0), .2f, 0, 0, false };
 		mats[2] = Material{ float4(1, 1, 1, 0), 0, 0, 0, false };
 		mats[3] = Material{ float4(1, 1, 1, 0), .5f, 0, 0, false };
-		mats[4] = Material{ float4(1, 1, 1, 0), 0, 1, 1.125f, true };
+		mats[4] = Material{ float4(1, 1, 1, 0), 0, 1, 1.1f, true };
+		mats[5] = Material{ float4(0, 1, 0, 0), 0, 0, 0, false };
+		mats[6] = Material{ float4(0, 0, 1, 0), 0, 0, 0, false };
+		mats[7] = Material{ float4(1, 1, .5f, 0), 0, 0, 0, false };
+
 
 		spheres[0] = Sphere{ float4(0, 0.f, -1.5f, 0.f), .25f, 2.f };			// 1: bouncing ball
-		spheres[1] = Sphere{ float4(3, 0.f, 0.f, 0), .25f, 2.f };			// 1: bouncing ball
+		spheres[1] = Sphere{ float4(1.5f, -0.49f, 0.f, 0), .25f, 2.f };			// 1: bouncing ball
 		prims[0] = Primitive{ 0, 0, 0 };
 		prims[1] = Primitive{ 0, 1, 4 };
 
@@ -267,14 +271,16 @@ public:
 		planes[3] = Plane{ float4(0, -1, 0, 0), 2.f };
 		planes[4] = Plane{ float4(0, 0, 1, 0), 9.f };
 		planes[5] = Plane{ float4(0, 0, -1, 0), 3.99f };
-		prims[2] = Primitive{ 1, 0, 2 };
-		prims[3] = Primitive{ 1, 1, 2 };
-		prims[4] = Primitive{ 1, 2, 2 };
-		prims[5] = Primitive{ 1, 3, 2 };
+		prims[2] = Primitive{ 1, 0, 5 };
+		prims[3] = Primitive{ 1, 1, 6 };
+		prims[4] = Primitive{ 1, 2, 7 };
+		prims[5] = Primitive{ 1, 3, 0 };
 		prims[6] = Primitive{ 1, 4, 2 };
 		prims[7] = Primitive{ 1, 5, 2 };
 
-		lights[0] = Light{ float4(2, 0, 3, 0), float4(1,1,.8f,0), 3, -1 };
+		lights[0] = Light{ float4(2, 0, 3, 0), float4(1,1,.8f,0), 2, -1 };
+		lights[1] = Light{ float4(1, 0, -5, 0), float4(1,1,.8f,0), 2, -1 };
+		lights[2] = Light{ float4(-2, 0, 3, 0), float4(1,1,.8f,0), 1, -1 };
 		//prims[6] = Primitive{ 1, 4, 0 };
 
 		SetTime( 0 );
@@ -301,8 +307,8 @@ public:
 	Sphere spheres[2];
 	Plane planes[6];
 	Cube cubes[1];
-	Material mats[5];
-	Light lights[1];
+	Material mats[8];
+	Light lights[3];
 	//Quad quad;
 	//Sphere sphere;
 	//Sphere sphere2;
